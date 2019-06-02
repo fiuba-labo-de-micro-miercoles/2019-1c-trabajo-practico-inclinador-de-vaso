ccb4c846729cf9b6cbed86875133b38cfff3d414
martinquattrone <48759672+martinquattrone@users.noreply.github.com>
2019-06-01 21:08:11 -0300

    Update main.asm

--- codigo/main.asm
+++ codigo/main.asm
@@ -51,13 +51,18 @@ configuracion_puertos:
 	sbi  PORTC, DOUT				; puerto PC = A1 como entrada (DOUT)
 	
 
+;-------------------------------------------------------------------------
+; lectura_peso: funcion para la lectura de datos de la celda de carga. Carga
+; los bits enviados por el amplificador HX711 a traves del pin DOUT y los
+; guarda en los registros r4:r2
+;-------------------------------------------------------------------------
 lectura_peso:
 	ldi  CONT_3, 3					; el dato ocupa 3 registros
 	ldi  zl, LOW(0x05)				; en r4:r2 guardo el dato
 	ldi  zh, HIGH(0x05)
 
 	sbic PINC, DOUT					
-	rjmp lectura_peso				; chequeo si DOUT est· en alto
+	rjmp lectura_peso				; chequeo si DOUT est√° en alto
 	nop
 	nop 					
 	sbic PINC, DOUT					; por la hoja de datos, DOUT debe estar como minimo 0.1 useg en 0
@@ -66,11 +71,11 @@ lectura_peso:
 here:
 	ldi  CONT_8, 8					; contador para la cantidad de bits por registro
 carga_bit:	
-	sbi  PORTC, SCK					; genero un flanco ascendente en la seÒal SCK para cargar un bit
+	sbi  PORTC, SCK					; genero un flanco ascendente en la se√±al SCK para cargar un bit
 	in   r16, PINC
 	bst  r16, DOUT					; en el flag T, tengo un bit del dato
 	bld  r17, 0						; lo guardo en LSB de r17
-	cbi  PORTC, SCK					; genero un flanco descendente en la seÒal SCK
+	cbi  PORTC, SCK					; genero un flanco descendente en la se√±al SCK
 	lsl  r17						; shifteo a la izq para dar lugar al proximo bit de dato
 	dec  CONT_8
 	brne carga_bit
