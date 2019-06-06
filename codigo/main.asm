9bf6f45e3baedf8582f4ed54c35dca4d55c648f0
Lola99254 <48759425+Lola99254@users.noreply.github.com>
2019-06-06 15:04:50 -0300

    Update main.asm

--- codigo/main.asm
+++ codigo/main.asm
@@ -32,6 +32,9 @@
 .def  DATO_M = r3
 .def  DATO_L = r2
 
+.def TARA_H	= r6
+.def TARA_L = r5
+
 ;-------------------------------------------------------------------------
 ; codigo
 ;-------------------------------------------------------------------------
@@ -53,23 +56,25 @@ main:
 
 
     rcall configuracion_puertos
-	rcall USART_init					
-		
-here:
+	rcall USART_init
+			
+	rcall set_tara
+	rcall dellay
+	rcall dellay
 
-	rcall lectura_peso
+here:
+	rcall lectura_peso				; lee los datos y los deja almacenados en r4:r2
 	rcall dellay
 	rcall dellay
-	rcall send_data
+	rcall send_data					; Se encarga de activar las interrupciones asi los datos son transmitidos por la UART
 	;rcall dellay
 	;rcall dellay
-	;rcall set_scale
+	;rcall set_scale				; Escala los valores almacenados en r4:r2
 	;rcall send_data
 	rcall dellay
 	rcall dellay
-
-	
-	rjmp here
+	cli
+ rjmp here
 
 ;-------------------------------------------------------------------------
 ; FUNCIONES
@@ -96,13 +101,13 @@ lectura_peso:
 	push  r16
 	push  r17
 
-loop:
+lectura_peso_loop:
 	sbic  PINC, DOUT					
-	rjmp  loop						; chequeo si DOUT está en alto
+	rjmp  lectura_peso_loop			; chequeo si DOUT está en alto
 	nop							
 	nop 							; Espero dos ciclos y vuelvo a preguntar si sigue en alto ya que
 	sbic  PINC, DOUT				; por la hoja de datos, DOUT debe estar como minimo 0.1 useg en 0
-	rjmp  lectura_peso				; para indicar que tiene un dato disponible para mandar.
+	rjmp  lectura_peso_loop			; para indicar que tiene un dato disponible para mandar.
 
 	rcall cargar_byte				; lee 8 bits y los guarda en r16
 	mov   DATO_H, r16				; primer byte es el mas significativo
@@ -118,7 +123,10 @@ loop:
 	nop
 	cbi   PORTC, SCK
 	
-	rcall set_scale					; Escala a gramos los datos recibidos
+	rcall com_2						; Hace el complemento a 2 de los datos leidos
+	
+	sub   DATO_L, TARA_L			; Le saco el offset a los valores
+	sbc   DATO_M, TARA_H
 
 	pop   r17
 	pop   r16
@@ -235,6 +243,24 @@ L3:
 	ret
 
 ; ----------------------------------------------------------------------
+; COM_2:
+; Elimina el byte menos significativo y hace el complemento a dos 
+; de los otros dos bytes y los deja en r3:r2 y clr r4. 
+; ----------------------------------------------------------------------
+
+com_2:
+
+	mov  DATO_L, DATO_M
+	mov  DATO_M, DATO_H
+	clr  DATO_H
+	
+	com DATO_M
+	neg DATO_L
+	brcs com_2_retornar
+	inc DATO_M
+com_2_retornar:
+	ret
+; ----------------------------------------------------------------------
 ; SET_SCALE:
 ; Con los datos de la balanza obtuvimos el factor de escala para pasar
 ; los datos a gramos. Como el factor es 1/6872 que se aproximó a 19/2^17
@@ -249,16 +275,13 @@ set_scale:
 	ldi r17, 7
 	ldi r16, MULTIPLICADOR
 
-	mov  DATO_L, DATO_M
-	mov  DATO_M, DATO_H
-	clr  DATO_H
-	
-	com DATO_M
-	neg DATO_L
-	brcs shifteo
-	inc DATO_M
-
 shifteo:
+	pop r19
+	pop r18
+	pop r17
+	pop r16
+	ret
+/*
 	lsr  DATO_M
 	ror  DATO_L
 	dec  r17
@@ -301,7 +324,7 @@ here_scale:
 	;pop r16
 	;ret
 	
-/*	
+
 	lsr  DATO_H										; Como hay que dividir por 2^17 simplemente se shiftea a la derecha el byte
 	neg  DATO_H										; mas significativo y luego se toma el complemento a 2.
 	
@@ -325,39 +348,45 @@ here_scale:
 ; me deja en TARA_H, TARA_L el peso leido inicialmente
 ; ----------------------------------------------------------------------
 	
-	.def TARA_H	= r6
-	.def TARA_L = r5
+	
 
+set_tara:
 	push r16
-	push r7
+	push r17
+	push r18
+	push r19
 
 	ldi  r16, 16				; contador
 	clr  TARA_L					; LOW_BYTE del resultado
 	clr  TARA_H					; HIGH_BYTE del resultado
-	clr  r7		
+	clr  r17
+	clr  r18
+	clr  r19		
 	
-
-loop_tara:
+set_tara_loop:
 	rcall lectura_peso
-	add  TARA_L, DATO_L			; dividir por 8 es shiftear 8 veces a la derecha o directamente agarrar el byte mas dignificativo del peso de 16 bits
-	adc  TARA_H, DATO_M
-	brcc next
-	inc  r7 					; si hubo carry incremento el HIGH_BYTE
-next:
+	add  r17, DATO_L			
+	adc  r18, DATO_M
+	brcc set_tara_next
+	inc  r19 					; si hubo carry incremento el HIGH_BYTE
+set_tara_next:
 	dec  r16
-	brne loop_tara
+	brne set_tara_loop
 	
-division_por_8:
-	ldi  r16, 4					; contador con 4 porque se va a shiftear 4 veces
-loop_division:
-	lsr  r7
-	ror  TARA_H
-	ror  TARA_L
+	ldi  r16, 4					; division por 16. Contador con 4 porque se va a shiftear 4 veces
+set_tara_division:
+	lsr  r19
+	ror  r18
+	ror  r17
 	dec  r16
-	brne  loop_division
+	brne  set_tara_division
+	
+	mov TARA_H, r18				; guardo en tara el promedio de los primeros 16 valores
+	mov TARA_L, r17
 
-	pop r16
-	pop r7
+	pop r19
+	pop r18
+	pop r17
+	pop r16	
 	ret
-
 ; ----------------------------------------------------------------------
